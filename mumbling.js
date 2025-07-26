0da23f5d7e527bf0fe6f1238b4301895e8f7bc3a
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-07-26 11:30:22 +0200

    new codewar

---
+++ mumbling.js
@@ -0,0 +1,11 @@
+function accum(s) {
+	let newArr = [];
+  let allToLowerCase = s.toLowerCase();
+  for(let i = 0;i<allToLowerCase.length;i++){
+    const char = allToLowerCase[i];
+    const repeatChar = char.repeat(i + 1);
+    const upperRepeat = repeatChar.charAt(0).toUpperCase() + repeatChar.slice(1);
+    newArr.push(upperRepeat)
+    }
+  return newArr.join('-');
+}
