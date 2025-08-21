43cb64e524892bf950e3375bb235f361861289d8
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-08-21 11:03:09 +0200

    this kata is for capitalizing elements by basing on their index

--- alternateCapitalization.js
+++ alternateCapitalization.js
@@ -1,4 +1,19 @@
-function capitalize(s){
-  return s.split("").filter((ele,i) => i % 2 === 0);
-};
-console.log(capitalize('aime'));
+const capitalize = (str) =>{
+    let earr = [];
+    let even = str.split("").forEach((ele,index) =>{
+        if(index % 2 === 0){
+            earr.push(ele.toUpperCase())
+        }else{
+            earr.push(ele)
+        }
+    })
+    let oarr = [];
+    let odd = str.split("").forEach((ele,index) =>{
+        if(index % 2 !== 0){
+            oarr.push(ele.toUpperCase())
+        }else{
+            oarr.push(ele)
+        }
+    })
+    return Array.of(earr.join(""),oarr.join(""));
+}
