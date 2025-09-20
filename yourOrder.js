f1e098407c26ab4e2732d9904872d8a4e421ea79
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2025-09-20 17:54:53 +0200

    yourOrder.js

--- yourOrder.js
+++ yourOrder.js
@@ -1,8 +1,4 @@
 const yourOrder = (str) =>{
-    let a = /\d/g;
-    let d = str.match(a).sort()
-    let c = str.split(" ").sort((a,b) => d[a] - d[b]);
-    return c;
-
+    return  str.split(" ").sort((a,b) => a.match(/\d/) - b.match(/\d/)).join(" ")
 }
-console.log(yourOrder("is2 Thi1s T4est 3a"))
+console.log(yourOrder("is2 Thi1s T4est 3a")) //Thi1s is2 3a T4est
