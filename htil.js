021caa265acb036d3bfb59e6ed67564fa7b0ea7d
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-01-14 20:55:57 +0200

    new kata

---
+++ htil.js
@@ -0,0 +1,16 @@
+export const head = (n) => {
+    return n[0];
+};
+export const tail = (n) => {
+    return n.splice(1, n.length - 1);
+};
+export const init = (n) => {
+    return n.splice(0, n.length - 1);
+};
+export const last = (n) => {
+    return n[n.length - 1];
+};
+console.log(head([1, 2, 3, 4, 5]));
+console.log(tail([1, 2, 3, 4, 5]));
+console.log(init([11, 10]));
+console.log(last([1, 2, 3, 4, 5]));
