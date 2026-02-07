662cda4c7ab1b2a8c5baeb9036e2566bfc4bf219
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-07 19:19:41 +0200

    suffix and prefix short string and reverse longest

---
+++ shorterConcat.js
@@ -0,0 +1,17 @@
+function shorterReverseLonger(a, b) {
+    if (a.length > b.length) {
+        a = a.split("").reverse().join("");
+        return b.concat(a).concat(b);
+        return "";
+    }
+    else if (b.length > a.length) {
+        b = b.split("").reverse().join("");
+        return a.concat(b).concat(a);
+    }
+    else {
+        a = a.split("").reverse().join("");
+        return b.concat(a).concat(b);
+    }
+}
+console.log(shorterReverseLonger("first", "abcde")); //abcdetsrifabcde
+console.log(shorterReverseLonger('nufan', 'pjcjfhzy'));
