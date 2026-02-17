90250d1817a12971784fd3419bcdf81f6be7adc9
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-17 17:20:13 +0200

    tail Swap

--- tailSwap.js
+++ tailSwap.js
@@ -1,15 +1,9 @@
 const tailSwap = (arr) => {
-  arr = arr.map((el) => [el]);
-  let q = arr.map((n) => {
-    n = n.join("").split("");
-    let b = n.indexOf(":");
-    let a = n.splice(b + 1, n.length);
-    n.pop();
-    return [n, a];
-  });
-  [q[0][0], q[0][1], q[1][0], q[1][1]] = [q[0][0], q[1][1], q[1][0], q[0][1]];
+  const splitArr = arr.map((el) => el.split(":"));
 
-  return q.map((el) => el.map((el) => el.join("")).join(":"));
+  [splitArr[0][1], splitArr[1][1]] = [splitArr[1][1], splitArr[0][1]];
+
+  return splitArr.map((el) => el.join(":"));
 };
-// console.log(a(["abc:123", "cde:456"]));
-console.log(tailSwap(["a:12345", "777:xyz"]));
+
+console.log(tailSwap(["abc:123", "cde:456"]));
