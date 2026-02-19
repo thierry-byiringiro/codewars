d2a724897c1d7e4bdf8ee8e07a8409264bdc67dd
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-19 09:47:55 +0200

    finished to do the encryption function

--- simpleEncryption.js
+++ simpleEncryption.js
@@ -1,6 +1,10 @@
-const a = (b) => {
-  let getOdds = b.split("").filter((_, el) => el % 2 !== 0);
-  let getEven = b.split("").filter((_, el) => el % 2 === 0);
-  return getOdds.join("") + "" + getEven.join("");
+const encrypt = (text, n) => {
+  if (n == 0) {
+    return text;
+  } else {
+    let getOdds = text.split("").filter((_, el) => el % 2 !== 0);
+    let getEven = text.split("").filter((_, el) => el % 2 === 0);
+    return encrypt(getOdds.join("") + "" + getEven.join(""), n - 1);
+  }
 };
-console.log(a("012345"));
+console.log(encrypt("This is a test!", 2)); // s eT ashi tist!  // s eT ashi tist!
