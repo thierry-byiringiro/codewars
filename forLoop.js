923ece3db029d5f9ed78300edaf3e94d054a63d1
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-04-28 23:12:22 +0200

    for loop

---
+++ forLoop.js
@@ -0,0 +1,11 @@
+function pickIt(arr){
+  let odd = [], even = [];
+  for(let i = 0;i < arr.length;i++){
+    if(arr[i] % 2 !== 0){
+      odd.push(arr[i])
+    }else{
+      even.push(arr[i])
+    }
+}
+  return [odd,even];
+} 
